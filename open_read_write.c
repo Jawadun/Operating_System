123680c0998706921b0ca65d9bffb6dec03b1610
Jawadun Noor <123160556+Jawadun@users.noreply.github.com>
2025-05-18 17:17:02 -0400

    Add files via upload

---
+++ open_read_write.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+int main()
+{
+    int n,fd;
+    char buf[50];
+    fd=open("test.txt",O_RDONLY);
+    n=read(fd,buf,10);
+    write(1,buf,10);
+}
